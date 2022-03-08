2eabddfde74eaf19b5d118376704c47a6bd9011b
Andresa <curso@coopersystem.local>
2022-03-08 20:06:14 -0300

    Navigation buttons

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,24 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
-import MovieStars from "components/MovieStars";
+
 
 function Listing() {
-   return (<> <Pagination />   <MovieStars /> </>); 
+   return (<> 
+      <Pagination />   
+         <div className="container">
+            <div className="row">
+               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"><MovieCard /> </div>
+               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"><MovieCard /> </div>
+               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"><MovieCard /> </div>
+               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"><MovieCard /> </div>
+               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"><MovieCard /> </div>
+              
+            </div>
+            
+         </div>
+      
+      </>
+      ); 
 }
 
 export default Listing;
